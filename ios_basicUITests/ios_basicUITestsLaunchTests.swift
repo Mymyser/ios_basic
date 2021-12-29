365fe73145009a95eb7a74427dcbfed20f8c3802
mymyser <dev001@mymyser.com>
2021-12-29 21:52:53 +0900

    iOS default app

---
+++ ios_basicUITests/ios_basicUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ios_basicUITestsLaunchTests.swift
+//  ios_basicUITests
+//
+//  Created by Mymyser Developer on 2021/11/27.
+//
+
+import XCTest
+
+class ios_basicUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
